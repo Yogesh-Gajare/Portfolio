ae2aec628fcc2f61384562f73098250a700a9524
Yogesh Gajare <yogeshygajare@gmail.com>
2024-05-19 19:56:37 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,19 @@
+# VaibhavKubde Portfolio (using HTML and CSS only)
+
+
+## Description
+This is a simple portfolio design created using HTML, CSS, and JavaScript to implement a responsive menu toggle feature, with theme color picker. The project is designed to showcase My work and provide a seamless user experience on both desktop and mobile devices.
+
+## Demo
+<a target="_blank" href="https://vaibhavkubde121.github.io/MyPortfolio/">Live Link</a>
+
+
+## Technologies Used 
+- HTML
+- CSS
+- Js(only for menu toggle in mobile view & theme color picker)
+
+
+
+---
+- 📫 How to reach me **vaibhavkubde2003@gmail.com** ❤️
---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+
+let menuIcon = document.querySelector('.menu-icon');
+let cancelIcon = document.querySelector('.cancel-icon');
+let headerLinks = document.querySelector('.header-links');
+
+menuIcon.addEventListener('click', function () {
+  headerLinks.style.display = 'block';
+  headerLinks.style.right = '0';
+});
+
+cancelIcon.addEventListener('click', function () {
+
+  headerLinks.style.right = '-100vh';
+  headerLinks.style.display = 'none';
+});
+
+
+let themeInput = document.getElementById('theme');
+const root = document.documentElement; 
+const primaryThemeColor = getComputedStyle(root).getPropertyValue('--primary-theme-color');
+themeInput.value = primaryThemeColor;
+
+themeInput.addEventListener('change', function () {
+  
+  const color = themeInput.value;
+  document.documentElement.style.setProperty('--primary-theme-color', color);
+});
+
+
+
+
